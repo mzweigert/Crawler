3a17da210d05036a77e8654caaeae78d8fd12b5e
mzweigert <mateuszzweigert@gmail.com>
2019-07-07 14:40:11 +0200

    Add main project packages structure.

---
+++ src/main/java/com/mzweigert/crawler/Runner.java
@@ -0,0 +1,8 @@
+package com.mzweigert.crawler;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/mzweigert/crawler/RunnerTest.java
@@ -0,0 +1,15 @@
+package com.mzweigert.crawler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RunnerTest {
+
+    @Test
+    public void test() {
+        assertThat(true).isTrue();
+    }
+}
